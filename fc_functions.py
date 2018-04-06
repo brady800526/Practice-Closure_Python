8172b2b17decc820dfe73c1bff65b7e51952126e
Brady <brady800526@gmail.com>
2018-04-06 14:03:24 +0800

    Closure

--- fc_functions.py
+++ fc_functions.py
@@ -1,3 +1,6 @@
+# First Class Function
+
+
 def html_tag(tag):
 
     def wrap_text(msg):
@@ -9,3 +12,36 @@ def html_tag(tag):
 print_h1 = html_tag('p')
 print_h1('Test Headline!')
 print_h1('Another Paragraph!')
+
+
+# Clousre
+
+
+import logging
+logging.basicConfig(filename='example.log', level=logging.INFO)
+
+
+def logger(func):
+    def log_func(*args):
+        logging.info('Running "{}" with arguments {}'.format(
+            func.__name__, args))
+        print(func(*args))
+    return log_func
+
+
+def add(x, y):
+    return x + y
+
+
+def sub(x, y):
+    return x - y
+
+
+add_logger = logger(add)
+sub_logger = logger(sub)
+
+add_logger(3, 3)
+add_logger(4, 5)
+
+sub_logger(10, 5)
+sub_logger(20, 10)
